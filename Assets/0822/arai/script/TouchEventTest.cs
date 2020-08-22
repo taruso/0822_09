afbccf4d2ca23265b31395dc742ec4536523c642
tarutaruso <watawata.sca@docomo.ne.jp>
2020-08-22 14:53:19 +0900

    araiついか

---
+++ Assets/0822/arai/script/TouchEventTest.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class TouchEventTest : MonoBehaviour, IPointerDownHandler
+{
+    private Drag mouse = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mouse = GameObject.Find("Mouse").GetComponent<Drag>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnPointerDown(PointerEventData data)
+    {
+        //ここに処理を書く。
+        //mouse.OnDrag(data);
+        Debug.Log("タッチしたよー！");
+    }
+}
