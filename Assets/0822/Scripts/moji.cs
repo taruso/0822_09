933699cb871ccdf1aaf435d39c0c1e5cd21ba140
tarutaruso <watawata.sca@docomo.ne.jp>
2020-08-22 14:41:26 +0900

    すてーじ追加

---
+++ Assets/0822/Scripts/moji.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class moji : MonoBehaviour, IDragHandler
+{
+    [SerializeField]
+    RectTransform rectTransform;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+ 
+        rectTransform.position += new Vector3(eventData.delta.x * 0.1f, eventData.delta.y * 0.1f, 0f);
+
+    }
+}
