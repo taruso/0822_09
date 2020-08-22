4676cf05827274ae6cef52bc2959edce631fca7c
arai <bythx790@gmail.com>
2020-08-22 13:29:54 +0900

    ドラッグ実装

---
+++ Assets/0822/arai/script/Drag.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class Drag : MonoBehaviour, IDragHandler
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+ 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //gameObject.transform.position = Input.mousePosition;
+    }
+
+    public void OnDrag(PointerEventData data)
+    {
+        Vector3 TargetPos = Camera.main.ScreenToWorldPoint(data.position);
+        TargetPos.z = 0;
+        transform.position = TargetPos;
+
+        // クリックしたスクリーン座標
+        var screenPoint = Input.mousePosition;
+
+        // Canvasにセットされているカメラを取得
+        var graphic = GetComponent<Graphic>();
+        var camera = graphic.canvas.worldCamera;
+
+        // Overlayの場合はScreenPointToLocalPointInRectangleにnullを渡さないといけないので書き換える
+        if (graphic.canvas.renderMode == RenderMode.ScreenSpaceOverlay)
+        {
+            camera = null;
+        }
+
+        // クリック位置に対応するRectTransformのlocalPositionを計算する
+        var localPoint = Vector2.zero;
+        RectTransformUtility.ScreenPointToLocalPointInRectangle(graphic.rectTransform, screenPoint, camera, out localPoint);
+
+        GetComponent<RectTransform>().localPosition = localPoint;
+    }
+}
